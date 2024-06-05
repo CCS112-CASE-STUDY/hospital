b4756e0187b5f67cea9f9f8a0c98b366f68cdf34
catherayn-github <143067796+catherayn-github@users.noreply.github.com>
2024-06-05 14:05:15 +0800

    doctor management back-end

--- back-end/app/Http/Controllers/DoctorController.php
+++ back-end/app/Http/Controllers/DoctorController.php
@@ -13,74 +13,132 @@ class DoctorController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function doctorList()
     {
-        //
-    }
+        $doctors = Doctor::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return response()->json([
+            'status' => 200,
+            'data' => $doctors
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreDoctorRequest  $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreDoctorRequest $request)
+    public function addDoctor(Request $request)
     {
-        //
+        // Define validation rules
+        $rules = [
+            'email' => 'required|email:rfc,dns'
+           
+        ];
+
+        // Validate the request data
+        $validator = Validator::make($request->all(), $rules);
+
+        // If validation fails, return a response with errors
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ]);
+        }
+    
+        $doctor = new Doctor();
+        $doctor->first_name = $request->first_name;
+        $doctor->last_name= $request->last_name;
+        $doctor->specialization = $request->specialization;
+        $doctor->license_number= $request->license_number;
+        $doctor->phone = $request->phone;
+        $doctor->email = $request->email;
+        $doctor->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Doctor added successfully'
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Doctor  $doctor
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Doctor $doctor)
+    public function displayDoctor(Request $request)
     {
-        //
-    }
+        $doctor = User::where('email', $request->email)->first();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Doctor  $doctor
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Doctor $doctor)
-    {
-        //
+        if (!$doctor) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Doctor is not yet registered'
+            ]);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'doctor' => $doctor
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateDoctorRequest  $request
-     * @param  \App\Models\Doctor  $doctor
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateDoctorRequest $request, Doctor $doctor)
+    public function editDoctor(Request $request, $id)
     {
-        //
+        $doctor= Doctor::find($id);
+
+        if (!$doctor) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Doctor is not yet registered'
+            ]);
+        }
+        $doctor->first_name = $request->first_name;
+        $doctor->last_name= $request->last_name;
+        $doctor->specialization = $request->specialization;
+        $doctor->license_number= $request->license_number;
+        $doctor->phone = $request->phone;
+        $doctor->email = $request->email;
+        $user->save();
+
+        return response()->json([
+            'status' => 201,
+            'message' => 'Doctor Information updated successfully'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Doctor  $doctor
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Doctor $doctor)
+    public function removeDoctor($id)
     {
-        //
+        $doctor = Doctor::find($id);
+
+        if (!$doctor) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'doctor not found'
+            ]);
+        }
+
+        $doctor->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Doctor removed successfully'
+        ]);
     }
 }
--- back-end/app/Models/Doctor.php
+++ back-end/app/Models/Doctor.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Doctor extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'doctors';
+    
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'specialization',
+        'license_number',
+        'phone',
+        'email'
+    ];
 }
