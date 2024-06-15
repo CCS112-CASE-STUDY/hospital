6d45ba703eb008646622a257f5b10a055dd29bf8
barnedodarrel <barnedodarrel11@gmail.com>
2024-06-15 18:22:12 +0800

    All related to the user registration and authentication front-end are added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserLogin from './component/UserLogin';
+import StaffPanel from './component/StaffPanel';
+import PatientRegistration from './component/PatientRegistration';
+import StaffRegistration from './component/StaffRegistration';
+import ManageUsers from './component/ManageUsers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {
+            // login route
+          }
+
+          <Route
+            exact
+            path='/'
+            element={<UserLogin />}
+          />
+
+          {
+            // admin routes
+          }
+
+          <Route
+            path='/hospital/admin/panel'
+            element={<StaffPanel />}
+          />
+
+          <Route
+            path='/hospital/admin/manage_user'
+            element={<ManageUsers />}
+          />
+
+          {
+            // patient routes
+          }
+
+          <Route
+            path='/hospital/patient/panel'
+            element={<PatientPanel />}
+          />
+
+          {
+            // doctor routes
+          }
+
+          <Route
+            path='/hospital/doctor/panel'
+            element={<StaffPanel />}
+          />
+
+          {
+            // receptionist routes
+          }
+
+          <Route
+            path='/hospital/receptionist/panel'
+            element={<StaffPanel />}
+          />
+
+          {
+            // registration routes
+          }
+
+          <Route
+            path='/user/patient/register'
+            element={<PatientRegistration />}
+          />
+
+          <Route
+            path='/user/staff/register'
+            element={<StaffRegistration />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
