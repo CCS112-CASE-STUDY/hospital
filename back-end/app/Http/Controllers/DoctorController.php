02dd34d6ce2dded71a085fe1d1d77883e261b7d8
Catherine Bello <bellocatherine022@gmail.com>
2024-06-15 20:30:11 +0800

    Doctor Back-end

---
+++ back-end/app/Http/Controllers/DoctorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Doctor;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    //
+}
